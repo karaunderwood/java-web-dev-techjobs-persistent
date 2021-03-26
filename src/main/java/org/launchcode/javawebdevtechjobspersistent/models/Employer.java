212ca0ce78428c4cba61409187841f7bc8850788
karaunderwood <karaunderwooddesign@gmail.com>
2021-03-25 21:40:07 -0500

    Finished Controllers section in Part 2

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,25 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank(message = "Location is required")
+    @Size(min = 3, max = 90, message = "Location must be between 3 and 90 characters")
+    private String location;
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Employer() {
+    }
 
 }
